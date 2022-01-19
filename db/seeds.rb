379ab1ca5fc541a94e5c70af96fdc7fbacf3352c
Laetitia Taddei <laetdi@gmail.com>
2022-01-19 14:47:25 +0900

    fixed bookmark new and create routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+puts "erasing existing datas"
+Movie.destroy_all
+
+puts "fetch api"
+url = "http://tmdb.lewagon.com/movie/top_rated?>"
+result = URI.open(url).read
+movies = JSON.parse(result)
+
+
+movies["results"].each do |movie|
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: movie['poster_path'],
+    rating: movie['vote_average']
+  )
+end
+
+puts "db seeded"
+# we have an array of hashes
+# we need to get our array with hash_result["results"]
+# each element of this array is an hash
+# eaxh hash contains a movie
