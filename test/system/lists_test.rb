44b5696fac7d1a2c7c19ed7c9352f0594ca6b149
Laetitia Taddei <laetdi@gmail.com>
2022-06-16 09:44:43 +0200

    testing presence of a name to create a list

--- test/system/lists_test.rb
+++ test/system/lists_test.rb
@@ -2,8 +2,23 @@ require "application_system_test_case"
 
 class ListsTest < ApplicationSystemTestCase
   test "visiting the index" do
+    #exercice
     visit root_url
-
+    #verify
     assert_selector "h1", text: "Let's watch something"
   end
+
+  test "create a new list" do
+    visit "/lists/new"
+
+    fill_in "list_name", with: "User"
+
+    click_on "Create"
+
+    # should be redirect to Home with new product
+    assert_equal root_path, page.current_path
+    assert_text "User"
+  end
+
+  test "update a list"
 end
