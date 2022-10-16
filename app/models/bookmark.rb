9e17c7a9a51c413265b6a7bf61447bde4173fff7
Laetitia Taddei <laetdi@gmail.com>
2022-10-16 18:47:26 +0200

    create spec file and start lists_controller specs

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   has_one_attached :photo
-  validates :comment, presence: true, length: { minimum: 7 }
   validates_uniqueness_of :movie, scope: :list
 end
